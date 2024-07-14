11ffb605cf6ec295d93c85093c1dd78e80043193
Lalithyapalla <pallalalithya25@gmail.com>
2024-07-14 18:15:51 +0000

    Sync LeetCode submission Runtime - 38 ms (0.01%), Memory - 16.5 MB (100.00%)

--- Python-Solutions/0066-plus-one/solution.py
+++ Python-Solutions/0066-plus-one/solution.py
@@ -1,8 +1,13 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        new=''
-        for num in digits:
-            new+=str(num)
-        res=int(new)+1
-        a=list(map(int,str(res)))
-        return a
+        # new=''
+        # for num in digits:
+        #     new+=str(num)
+        # res=int(new)+1
+        # a=list(map(int,str(res)))
+        # return a
+
+        string=reduce(lambda val1,val2:val1+str(val2), digits,'')
+        #new=string[:-1]+str(int(string[-1])+1)
+        new=int(string)+1
+        return [int(val) for val in str(new)]
