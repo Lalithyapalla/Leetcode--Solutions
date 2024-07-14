533beb144f0bcf74d8a8d56f997010c29ab499f0
Lalithyapalla <pallalalithya25@gmail.com>
2024-07-14 16:00:28 +0000

    Sync LeetCode submission Runtime - 6219 ms (5.35%), Memory - 17 MB (100.00%)

--- Python-Solutions/0242-valid-anagram/solution.py
+++ Python-Solutions/0242-valid-anagram/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        l=len(s)
-       # l1=len(t)
-        c=0
-        if len(s)==len(t):
-            for ch in s:
-                if ch in t:
-                    c+=1
-                    s=s.replace(ch,'',1)
-                    t=t.replace(ch,'',1)
+        return {char:s.count(char) for char in s}=={char:t.count(char) for char in t}
+    #     l=len(s)
+    #    # l1=len(t)
+    #     c=0
+    #     if len(s)==len(t):
+    #         for ch in s:
+    #             if ch in t:
+    #                 c+=1
+    #                 s=s.replace(ch,'',1)
+    #                 t=t.replace(ch,'',1)
 
-            return c==l
-        return False
+    #         return c==l
+    #     return False
+
             
