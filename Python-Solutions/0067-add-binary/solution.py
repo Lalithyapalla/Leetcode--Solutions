824b454caf68641170a956bda10cbb95275fb7d8
Lalithyapalla <pallalalithya25@gmail.com>
2024-07-14 18:37:15 +0000

    Sync LeetCode submission Runtime - 36 ms (5.76%), Memory - 17.1 MB (100.00%)

--- Python-Solutions/0067-add-binary/solution.py
+++ Python-Solutions/0067-add-binary/solution.py
@@ -1,8 +1,27 @@
 class Solution:
-    def binary(self,num,pos=0):
-            if num==0:
-                return 0
-            return num%10*(2**pos)+self.binary(num//10,pos+1)
+    # def binary(self,num,pos=0):
+    #         if num==0:
+    #             return 0
+    #         return num%10*(2**pos)+self.binary(num//10,pos+1)
+    # def addBinary(self, a: str, b: str) -> str:
+    #     res=self.binary(int(a))+self.binary(int(b))
+    #     return bin(res)[2:]
+
+    def binary(self,a,pos=0):
+        if a==0:
+            return 0
+        return (a%10)*(2**pos)+self.binary(a//10,pos+1)
     def addBinary(self, a: str, b: str) -> str:
         res=self.binary(int(a))+self.binary(int(b))
-        return bin(res)[2:]
+        return self.convert_dec(res)
+    
+    def convert_dec(self,num): 
+        if num==0:
+            return '0'
+        res=''
+        while num>0:
+            val=num%2
+            res=str(val)+res
+            num//=2
+        return res
+
