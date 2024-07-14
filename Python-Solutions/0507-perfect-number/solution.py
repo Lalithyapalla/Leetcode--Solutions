cc641b5c09f45020fe2ebe34d14a1b84abc408e3
Lalithyapalla <pallalalithya25@gmail.com>
2024-07-14 18:05:39 +0000

    Sync LeetCode submission Runtime - 38 ms (6.04%), Memory - 16.6 MB (100.00%)

--- Python-Solutions/0507-perfect-number/solution.py
+++ Python-Solutions/0507-perfect-number/solution.py
@@ -1,5 +1,8 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
+        # res=list(filter(lambda fa:num%fa==0, range(1,num//2+1)))
+        # div_sum=reduce(lambda a,b:a+b,res,0)
+        # return div_sum==num
        if num<=1:
         return False
        n_sum=1
@@ -9,3 +12,10 @@ class Solution:
             if val!=num//val:
                 n_sum+=num//val
        return n_sum==num
+
+    # def perfect(self,num,fa=1):
+    #     if fa==num:
+    #         return 0
+    #     return fa+self.perfect(num,fa+1) if (num%fa==0) else self.perfect(num,fa+1)
+
+        
