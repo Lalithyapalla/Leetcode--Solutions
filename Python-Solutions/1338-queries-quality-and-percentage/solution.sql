27ac2a55eee1ea131e6f1f931703e3ee01bbf4df
Lalithyapalla <pallalalithya25@gmail.com>
2026-05-31 17:29:28 +0000

    Sync LeetCode submission Runtime - 352 ms (83.90%), Memory - 0.0B (100.00%)

---
+++ Python-Solutions/1338-queries-quality-and-percentage/solution.sql
@@ -0,0 +1,8 @@
+select query_name, 
+ROUND(AVG(rating / position), 2) AS quality,
+ ROUND(AVG(IF(rating < 3, 1, 0)) * 100, 2) AS poor_query_percentage from Queries where
+query_name IS NOT NULL
+GROUP BY query_name;
+
+
+
