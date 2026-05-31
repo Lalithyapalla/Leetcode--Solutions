fa40bda7bc2fb595a9e8ec199ae4137c7138ccf0
Lalithyapalla <pallalalithya25@gmail.com>
2026-05-31 18:53:13 +0000

    Sync LeetCode submission Runtime - 3 ms (72.55%), Memory - 20.1 MB (46.20%)

---
+++ Python-Solutions/0979-di-string-match/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        low = 0
+        high = len(s)
+        res = []
+        
+        for char in s:
+            if char == 'I':
+                res.append(low)
+                low += 1
+            else:
+                res.append(high)
+                high -= 1
+
+        res.append(low) 
+        return res
