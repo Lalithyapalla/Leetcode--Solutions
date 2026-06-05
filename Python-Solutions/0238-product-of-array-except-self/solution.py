ca6680ae9db2dd082eff9123292878f62918fd91
Lalithyapalla <pallalalithya25@gmail.com>
2026-06-05 02:41:24 +0000

    Sync LeetCode submission Runtime - 19 ms (76.68%), Memory - 25.5 MB (55.22%)

---
+++ Python-Solutions/0238-product-of-array-except-self/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = 1
+        n = len(nums)    #[1,2,3,4]
+        res = [1] * n   #[1,1,1,1]
+        for i in range(n):
+            res[i] = prefix
+            prefix *= nums[i]
+        suffix = 1
+        for i in range(n-1, -1, -1):
+            res[i] *= suffix
+            suffix *= nums[i]
+        return res
