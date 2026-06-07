ab969624f5b5555cd469aeb66d7709d8e7c11735
Lalithyapalla <pallalalithya25@gmail.com>
2026-06-07 03:22:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (53.94%)

---
+++ Python-Solutions/0050-powx-n/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1 / x
+            n = -n
+        res = 1
+        prod = x
+        while n > 0:
+            if n % 2 == 1:
+                res = res * prod
+            prod = prod * prod
+            n = n // 2 
+        return res
+
+        """return x**n"""
