a57181aa9aec97fe1515251d991f3d3933696dea
Lalithyapalla <pallalalithya25@gmail.com>
2026-06-07 02:57:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (54.50%)

---
+++ Python-Solutions/1236-n-th-tribonacci-number/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        prev3, prev2, prev1 = 0, 1, 1
+        for i in range(3, n+1):
+            cur = prev1 + prev2 + prev3
+            prev3 = prev2
+            prev2 = prev1
+            prev1 = cur
+        return prev1
