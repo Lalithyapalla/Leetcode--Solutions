576db0eb5c08645b3d081dcea441bd398b731efa
Lalithyapalla <pallalalithya25@gmail.com>
2026-06-21 21:32:32 +0530

    Time: 0 ms (100%), Space: 19.4 MB (39.76%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,11 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        res = {}
+        for num in arr:
+            if num not in res:
+                res[num] = 1
+            else:
+                res[num] += 1
+                #{1:3,2:2,3:2}
+        keys = len(res.values())
+        return keys == len(set(res.values()))
