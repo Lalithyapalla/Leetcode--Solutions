6a001d10f20dbde05a63f70113068d9cfe1cade7
Lalithyapalla <pallalalithya25@gmail.com>
2026-06-24 10:29:54 +0530

    Time: 0 ms (100%), Space: 19.3 MB (67.53%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        # ['a', 'b', 'b, 'c']
+        #   |    |    |    |
+        # [dog, cat, cat, dog]
+        s_list = s.split()
+        if len(pattern) != len(s_list):
+            return False
+        d = {}
+        for i, j in zip(pattern, s_list):
+            if i not in d:
+                d[i] = j
+            else:
+                if d[i] != j:
+                    return False
+                else:
+                    continue
+        ss = set()
+        for val in d:
+            ss.add(d[val])
+        return len(ss) == len(d)
+        # for multiple variables of pattern with same values
