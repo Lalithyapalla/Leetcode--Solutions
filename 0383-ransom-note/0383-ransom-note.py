45d6a5d2d6ac326c74a0e0bbbd329d128d7c67e2
Lalithyapalla <pallalalithya25@gmail.com>
2026-06-24 09:06:50 +0530

    Time: 6 ms (94.41%), Space: 19.5 MB (71.59%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,9 +1,12 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        mag_d = {char:magazine.count(char) for char in set(magazine) }
+       
+        
+        mag = set(magazine)
+        ans = {char: magazine.count(char) for char in mag}
         for char in ransomNote:
-            if char in mag_d and mag_d[char] > 0:
-                mag_d[char] -= 1
+            if char in ans and ans[char] > 0 :
+                ans[char] -= 1
             else:
                 return False
         return True
